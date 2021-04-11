16bd650ccd626cead3da3f85aa4b1b64494ad199
noko1024 <ynoko1024@gmail.com>
2021-04-12 00:08:32 +0900

    update

--- DiscordOAuth.py
+++ DiscordOAuth.py
@@ -1,5 +1,7 @@
 import requests
 from flask import Flask, json,request,make_response,jsonify,redirect,render_template
+#apacheパイセンがよしなになってくれそう
+#from flask_sslify import SSLify
 import os
 import sqlite3
 import qrcode
@@ -10,6 +12,7 @@ from io import BytesIO
 
 from requests import status_codes
 
+#SSL関係はあぱっちパイセンに任せてるのでローカル環境ではcode 400 Bad Request
 
 #---config.json読み込み---
 configPath = os.path.join(os.path.split(os.path.realpath(__file__))[0],"config")
@@ -23,18 +26,22 @@ ClientSecret = "JL5nbj6qCyO3j77R-eox7IHeXNly6msU"
 botToken = "Njc4Njk0MjA5MDU3OTgwNDY3.XkmhPA.hiCytsZowRx3sm59Al2AazYR4MM"
 guildID = "830565829895782440"
 roleID = "830566515392249857"
-redirectURL = "https://discord.com/api/oauth2/authorize?client_id=678694209057980467&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2FOAuth&response_type=code&scope=identify%20guilds"
+redirectURL = "https://discord.com/api/oauth2/authorize?client_id=678694209057980467&redirect_uri=https%3A%2F%2Fapi.noko1024.net%2FOAuth&response_type=code&scope=identify%20guilds"
 
 oneTimeURL = []
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += "HIGH:!DH:!aNULL"
 app = Flask(__name__)
+
+#sslify = SSLify(app)
+
 basePath = os.path.dirname(__file__)
 
 @app.route("/gen")
 def OneTimeURLGenerate():
+    global oneTimeURL
     onetime = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-    accessURL = "https://api.noko1024.net/want/"+onetime
+    accessURL = "https://api.noko1024.net/want?param="+onetime
     oneTimeURL.append(onetime)
     img = qrcode.make(accessURL)
 
@@ -45,7 +52,7 @@ def OneTimeURLGenerate():
     qr_b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
     # image要素のsrc属性に埋め込めこむために、適切に付帯情報を付与する
     qr_b64data = "data:image/png;base64,{}".format(qr_b64str)
-    print(accessURL)
+    print("GEN-ONETIMEURL")
     print(oneTimeURL)
     return render_template("QR.html",qr_b64data=qr_b64data)
 
@@ -53,9 +60,14 @@ def OneTimeURLGenerate():
 
 
 
-@app.route("/want/<accessOpt>")
-def AuthWait(accessOpt):
+@app.route("/want")
+def AuthWait():
+    global oneTimeURL
+    accessOpt = request.args.get('param', default = None, type = str)
+    print("Want-Access")
     print(accessOpt)
+    print("Want-onetimeurl")
+    print(oneTimeURL)
     if accessOpt in oneTimeURL:
         oneTimeURL.remove(accessOpt)
         return redirect(redirectURL)
@@ -76,7 +88,7 @@ def OAuth():
         'client_secret': ClientSecret,
         'grant_type': 'authorization_code',
         'code': code,
-        'redirect_uri': "http://localhost:5000/OAuth",
+        'redirect_uri': "https://api.noko1024.net/OAuth",
         'scope': 'identify guilds'
     }
     headers_token = {
@@ -96,7 +108,7 @@ def OAuth():
 
     #所属サーバー一覧を取得(List内部にdict)
     userGuild :json = requests.get("https://discord.com/api/users/@me/guilds", headers=headers).json()
-    print(userGuild)
+    #print(userGuild)
 
     #トークン破棄用
     headers_token = {
@@ -117,10 +129,11 @@ def OAuth():
         return "NOTPROKEN"
 
     #HTTPS使えない説　削除成功しているか？
-    if requests.delete("https://discord.com/api/guilds/"+guildID+"/members/"+userInfo["id"]+"/roles/"+roleID,headers=headers).status_code == 204:
+    status = requests.delete("https://discord.com/api/guilds/"+guildID+"/members/"+userInfo["id"]+"/roles/"+roleID,headers=headers).status_code
+    if status == 204:
         return "成功しました。"
 
-    return userInfo
+    return status
 
 if __name__ == "__main__":
 	app.run(debug=True)
